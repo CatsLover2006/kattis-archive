95cd71e1915926f563a7e608cfdbcc0834a3502c
Hail <chancelorne@icloud.com>
2022-10-28 13:21:43 -0600

    better stopwatch python solution

--- stopwatch.py
+++ stopwatch.py
@@ -1,9 +1,9 @@
 l=int(input())
-if(l%2):
+t=0
+if l%2:
 	print"still running"
 	exit()
-t=0
-while(l>0):
-	t-=int(input())-int(input())
-	l-=2
+while l>0:
+	l-=1
+	t-=int(input())*((l%2)*2-1)
 print t
